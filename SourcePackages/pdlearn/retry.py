9ec2c501b3c40d986ce8e7f8d5d86c645a86bd01
Makiras <makiras.x@outlook.com>
2022-09-16 01:31:58 +0800

    speed optimize

--- SourcePackages/pdlearn/retry.py
+++ SourcePackages/pdlearn/retry.py
@@ -1,6 +1,8 @@
 import requests 
+import functools
 from requests.adapters import HTTPAdapter
 from urllib3.util import Retry
+
 
 def requestsR(
     retries=3,
@@ -20,4 +22,5 @@ def requestsR(
     session.mount('http://', adapter)
     session.mount('https://', adapter)
     session.headers.update({'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36', 'Cache-Control': 'no-cache'})
+    session.request = functools.partial(session.request, timeout=(2.05, 4.05))
     return session
